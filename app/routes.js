0f95b4ac8b30ce64ca6b4fccf4c4737ae3e79cb3
jacobaloysious <jacobaloysious@gmail.com>
2016-02-04 09:49:32 +0530

    Update metadata, and added procfile for heroku

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,13 @@ module.exports = function(app) {
 	// server routes ===========================================================
 	// handle things like api calls
 	// authentication routes
+    app.get('/ping', function(req, res) {
+        res.send(200, {text: "All good. You don't need to be authenticated to call this"});
+    });
+
+    app.get('/secured/ping', function(req, res) {
+        res.send(200, {text: "All good. You only get this message if you're authenticated"});
+    });
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
--- server.js
+++ server.js
@@ -3,9 +3,19 @@
 // modules =================================================
 var express        = require('express');
 var app            = express();
+var cors           = require('cors');
 //var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
+var jwt            = require('express-jwt');
+//var dotenv         = require('dotenv');
+
+//dotenv.load();
+
+var authenticate = jwt({
+  secret: new Buffer('ntI0YoKsC9YRXm7ZawU2ossNXxggHAjaoonM4a3SUpvMponcYBJfQG_l1iwrefHd', 'base64'),
+  audience: 'gkgiKHGp6H1ps4xISZs2kKs1jGFNepGd'
+});
 
 // configuration ===========================================
 	
@@ -14,6 +24,10 @@ var db = require('./config/db');
 
 var port = process.env.PORT || 3080; // set our port
 // mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
+
+app.use(cors());
+
+app.use('/secured', authenticate);
 
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json 
